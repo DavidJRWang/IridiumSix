c49f1707a08b8510c344368f319eaf2f4dfc5434
daviwang <daviwang@umich.edu>
2019-08-03 22:47:37 -0400

    Added necessary string arrays for use with spinners. Improved layout. Layout is not finalized yet.

--- app/src/main/java/com/davidwang/unitconverter/MainActivity.java
+++ app/src/main/java/com/davidwang/unitconverter/MainActivity.java
@@ -2,6 +2,8 @@ package com.davidwang.unitconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner topSpinner = (Spinner) findViewById(R.id.top_spinner);
+        Spinner bottomSpinner = (Spinner) findViewById(R.id.bottom_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.distance_units, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        topSpinner.setAdapter(adapter);
+        bottomSpinner.setAdapter(adapter);
     }
 }
