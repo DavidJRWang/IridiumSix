e5daef8e4c571fff1a0ff137ec55442c0307d0fc
daviwang <daviwang@umich.edu>
2019-08-04 22:44:45 -0400

    Added listeners for selection on the spinner objects. Processes for after an item is selected still need to be determined.

--- app/src/main/java/com/davidwang/unitconverter/MainActivity.java
+++ app/src/main/java/com/davidwang/unitconverter/MainActivity.java
@@ -2,6 +2,8 @@ package com.davidwang.unitconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -12,12 +14,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Attach spinner views to member variables
         Spinner topSpinner = (Spinner) findViewById(R.id.top_spinner);
         Spinner bottomSpinner = (Spinner) findViewById(R.id.bottom_spinner);
 
+        // Create and set adapters for spinners
+        // The same one can be used as you compare distance units to distance units, etc.
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.distance_units, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         topSpinner.setAdapter(adapter);
         bottomSpinner.setAdapter(adapter);
+
+        topSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                // An item was selected. You can retrieve the selected item using
+                // parent.getItemAtPosition(pos)
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callback
+            }
+        });
+        bottomSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                // An item was selected. You can retrieve the selected item using
+                // parent.getItemAtPosition(pos)
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callback
+            }
+        });
     }
 }
